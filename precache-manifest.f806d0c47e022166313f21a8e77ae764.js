c50b6a202f08f8322caedc4a26d7f63917b61792
galtnie <hereby@protonmail.com>
2019-05-22 15:00:15 +0300

    Updates

---
+++ precache-manifest.f806d0c47e022166313f21a8e77ae764.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "443d19b77222f124f8cf9109aa4a0d60",
+    "url": "/booking_test/index.html"
+  },
+  {
+    "revision": "98bd96ae3ce3917a082f",
+    "url": "/booking_test/static/css/main.69e905f3.chunk.css"
+  },
+  {
+    "revision": "ce0dea846a89b9200fc5",
+    "url": "/booking_test/static/js/2.68d5a176.chunk.js"
+  },
+  {
+    "revision": "98bd96ae3ce3917a082f",
+    "url": "/booking_test/static/js/main.f1cc75b9.chunk.js"
+  },
+  {
+    "revision": "5ecbfbf617d6b5d77cc3",
+    "url": "/booking_test/static/js/runtime~main.ce44fa32.js"
+  }
+]);
